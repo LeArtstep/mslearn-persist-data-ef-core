c5f9382a6d9bbcdc9033e7506074e3ed8e866f32
Sash <130072394+LeArtstep@users.noreply.github.com>
2024-02-05 14:36:14 +0000

    Looks good

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,16 +1,18 @@
+using ContosoPizza.Data;
 using ContosoPizza.Services;
 // Additional using declarations
-
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 // Add the PizzaContext
-
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 // Add the PromotionsContext
-
+builder.Services.AddSqlite<PromotionsContext>("Data Source=Promotions/Promotions.db");
 builder.Services.AddScoped<PizzaService>();
 
 var app = builder.Build();
@@ -27,7 +29,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Add the CreateDbIfNotExists method call
-
+app.CreateDbIfNotExists();
 app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
 app.Run();
